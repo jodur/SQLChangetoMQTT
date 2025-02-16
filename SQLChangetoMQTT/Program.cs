4ba7f97fc41a0f3a8af4be45ff02747bb3df9dc6
Jörgen van Dure <durenjv@hotmail.com>
2025-02-16 08:16:25 +0100

    First version without appsettings

--- SQLChangetoMQTT/Program.cs
+++ SQLChangetoMQTT/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text.Json;
 using Microsoft.Data.SqlClient;
 using System.Threading.Tasks;
 using MQTTnet;
 
 class Program
 {
-    private static string connectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;";
-    private static string query = "YourComplexSQLQueryHere";
+    private static string connectionString = "Server=localhost;Database=pubs;User Id=engineer;Password=engineer;TrustServerCertificate=True;";
+    private static string query = "SELECT au_id, au_lname, au_fname FROM [dbo].[authors]";
     private static IMqttClient? mqttClient;
 
     static async Task Main(string[] args)
@@ -16,7 +19,7 @@ class Program
         mqttClient = factory.CreateMqttClient();
         var options = new MqttClientOptionsBuilder()
             .WithClientId("ClientID")
-            .WithTcpServer("broker.hivemq.com", 1883)
+            .WithTcpServer("192.168.88.1", 1883)
             .Build();
 
         await mqttClient.ConnectAsync(options, CancellationToken.None);
@@ -33,13 +36,15 @@ class Program
         await mqttClient.DisconnectAsync();
     }
 
+    private static SqlDependency? dependency;
+
     private static async Task RegisterSqlDependency()
     {
         using (SqlConnection connection = new SqlConnection(connectionString))
         {
             using (SqlCommand command = new SqlCommand(query, connection))
             {
-                SqlDependency dependency = new SqlDependency(command);
+                dependency = new SqlDependency(command);
                 dependency.OnChange += new OnChangeEventHandler(OnDatabaseChange);
 
                 await connection.OpenAsync();
@@ -59,10 +64,9 @@ class Program
 
             // Transmit new data via MQTT
             var message = new MqttApplicationMessageBuilder()
-                .WithTopic("your/topic")
+                .WithTopic("MSQL/Data")
                 .WithPayload(newData)
                 .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.ExactlyOnce)
-
                 .WithRetainFlag()
                 .Build();
 
@@ -79,6 +83,9 @@ class Program
 
     private static async Task<string?> FetchNewData()
     {
+        var results = new List<Dictionary<string, object>>();
+        var timestamp = DateTime.UtcNow;
+
         using (SqlConnection connection = new SqlConnection(connectionString))
         {
             using (SqlCommand command = new SqlCommand(query, connection))
@@ -86,14 +93,26 @@ class Program
                 await connection.OpenAsync();
                 using (SqlDataReader reader = await command.ExecuteReaderAsync())
                 {
-                    // Assuming the query returns a single row and single column
-                    if (await reader.ReadAsync())
+                    while (await reader.ReadAsync())
                     {
-                        return reader[0]?.ToString();
+                        var row = new Dictionary<string, object>();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[reader.GetName(i)] = reader.GetValue(i);
+                        }
+                        results.Add(row);
                     }
                 }
             }
         }
-        return null;
+
+        var payload = new
+        {
+            Timestamp = timestamp,
+            RecordCount = results.Count,
+            Data = results
+        };
+
+        return JsonSerializer.Serialize(payload);
     }
 }
